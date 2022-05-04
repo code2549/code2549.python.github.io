4aaec126b8e36f6cf4b0187dfc1bdcf25d839cb2
郝文杰haowenjie <100186691+code2549@users.noreply.github.com>
2022-05-04 22:17:56 +0800

    Add files via upload

---
+++ 进制转换.py
@@ -0,0 +1,25 @@
+# 其他进制转十进制
+# 二进制转十进制
+two = input('请输入您要转为十进制的数字')
+print(int(two, 2))
+
+# 八进制转十进制
+eight = input('请输入您要转换为十进制的数字')
+print(int(eight, 8))
+
+# 十六进制转十进制
+sixteen = input('请输入您要转换为十进制的数字')
+print(int(sixteen, 16))
+
+# 十进制转其他进制
+# 十进制转十六进制
+tentosixteen = input('请输入您要转换为十六进制的数字')
+print(hex(tentosixteen))
+
+# 十进制转换为二进制
+tentotwo = input('请输入您要转换为二进制的数字')
+print(bin(tentotwo))
+
+# 十进制转八进制
+tentoeight = input('请输入您要转换为八进制的数字')
+print(oct(tentoeight))
