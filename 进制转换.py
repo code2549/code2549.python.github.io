a30ee9b01773b24633aa7e374b930c2652848284
郝文杰haowenjie <100186691+code2549@users.noreply.github.com>
2022-05-05 23:31:33 +0800

    Add files via upload

--- 进制转换.py
+++ 进制转换.py
@@ -23,3 +23,15 @@ print(bin(tentotwo))
 # 十进制转八进制
 tentoeight = input('请输入您要转换为八进制的数字')
 print(oct(tentoeight))
+
+
+# 任意进制转换
+snum = input('请选择要转换的进制数\n 1.二进制\n 2.十进制\n 3.八进制\n 4.十六进制')
+if snum == '1':
+    num2 = input('请输入一个二进制数字')
+if snum == '2':
+    num10 = input('请输入一个十进制数字')
+if snum == '3':
+    num8 = input('请输入一个八进制数字')
+if snum == '4':
+    num16 = input('请输入一个十六进制的数字')
